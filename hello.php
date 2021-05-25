4ac5d99a059dd444662c4240f9f169cfd40ea372
Leslie <lesliec38@hotmail.fr>
2021-05-25 14:26:52 +0200

    modification fichier pour quetes -> variables

--- hello.php
+++ hello.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Hello Wilder!';
+// nom du film
+$movieName = 'Indiana Jones et la dernière croisade';
+// film visionné ou non
+$movieViewed = 'Of course';
+// année de sortie en salle
+$movieLaunch = '18 October 1989 (France)';
+// note du fim
+$movieRate = '8.2';
+
+echo "'$movieName' was first launch the $movieLaunch and received $movieRate out of 10. Did I see it ? $movieViewed";
