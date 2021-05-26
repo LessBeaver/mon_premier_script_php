ab2421d83e60cf76a72671bc8b3f3ad1213cf823
Leslie <lesliec38@hotmail.fr>
2021-05-26 12:47:51 +0200

    MAJ index.php

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 
 // Les aventuriers de la fonction perdue
 
-$params1 = 'La lune';
-$params2 = 'Raton le raton';
+// Version avec texte
+// $params1 = 'La lune';
+// $params2 = 'Raton le raton';
+
+// Version sans texte
+$params1 = '';
+$params2 = '';
 
 function writeSecretSentence(string $params1, string $params2): string
 {
